c97f62ca0579dbcdc277b938729e8c32fed9f7f7
opl- <opl-@users.noreply.github.com>
2023-02-03 21:22:30 +0100

    Fix Hub not calling super fuctions, remove currency amounts from hubs

--- src/building/Hub.gd
+++ src/building/Hub.gd
@@ -1,18 +1,8 @@
 class_name Hub
 extends "res://src/building/Building.gd"
 
-var currency_amount = {}
-
 func _ready():
-	pass
+	super._ready()
 
-func _process(delta):
-	pass
-
-func get_currency_amount(currency: Const.Currency) -> float:
-	return currency_amount[currency]
-
-func increment_currency_amount(currency: Const.Currency, delta: float) -> float:
-	var amount = currency_amount[currency] + delta
-	currency_amount[currency] = amount
-	return amount
+func _process(delta: float):
+	super._process(delta)
